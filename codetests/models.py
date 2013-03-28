1a0810704592722d493b3f2019deceeca972e58f
Miroslava Sotakova <mirka@knewton.com>
2013-03-28 17:56:38 -0400

    Done for today

--- codetests/models.py
+++ codetests/models.py
@@ -14,21 +14,21 @@ class Problem(models.Model):
     # upper bound for run time, checking efficiency
     max_duration = models.IntegerField()
 
-class Document(models.Model):
-    docfile = models.FileField(upload_to='sources/%s')
-
 class UploadForm(forms.Form):
     sender = forms.EmailField()
     problem_id = forms.MultipleChoiceField(
         widget = forms.CheckboxSelectMultiple, 
         choices = PROBLEM_CHOICES)
     message = forms.CharField()
-    docfile = forms.FileField(
-        label = 'Select a file',
-        help_text = 'max. 1MB',
-        # should be true but has validation issues
-        required = False,
-    )
+    gzfile = forms.FileField(
+                label = 'Select a file',
+                help_text = 'max. 1MB',
+                # should be true but has validation issues
+                required = False,
+             )
 
     def __unicode__(self):
         return self.sender()
+
+class Document(models.Model):
+    gzfile = models.FileField(upload_to = '/tmp/')
--- codetests/views.py
+++ codetests/views.py
@@ -6,12 +6,18 @@ from django.template import RequestContext
 from codetests.models import UploadForm
 from codetests.models import Document
 
+def handle_file(newfile):
+    if file:
+        destination = open('/tmp/' + newfile.name, 'wb+')
+        for chunk in newfile.chunks():
+            destination.write(chunk)
+        destination.close()
+
 def upload(request):
     if request.method == 'POST': # If the form has been submitted...
         form = UploadForm(request.POST, request.FILES) # A form bound to the POST data
         if form.is_valid():
-            newfile = Document(docfile = request.FILES['docfile'])
-            newdoc.save()
+            handle_file(request.FILES['gzfile'])
             return HttpResponseRedirect('/response/')
     else:
         form = UploadForm() # An unbound form
@@ -20,6 +26,6 @@ def upload(request):
 
 def response(request):
     if (1 == 1):
-        return render(request, 'response.html', { 'answer' : 'Good job', })
+        return render(request, 'response.html', { 'answer' : 'Good job' })
     else:
         return render(request, 'upload.html', { 'form' : UploadForm(), })
